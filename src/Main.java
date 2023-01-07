3a8373244fb72ecdfd063333118521b2290681aa
Poloski Cordon <poloski.cordon@gmail.com>
2023-01-07 15:30:04 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Poloski Cordon");
+        System.out.println("CIT260-02");
+        System.out.println("Guatemala City, Guatemala");
+        System.out.println("Ice cream");
+    }
+}
